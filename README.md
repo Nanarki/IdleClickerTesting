a3f306a1b49b8428d986bc1a3a11d5799adf0d68
Nanarki <40344615+Nanarki@users.noreply.github.com>
2018-06-18 15:26:12 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IdleClickerTesting
 Basic testing for a web based clicker. Not expected to look good, mainly just a sandbox/ testing page
+
+The code in the game uses Bootstrap for layout purposes
--- main.js
+++ main.js
@@ -8,10 +8,10 @@ var manualUpgrades = 0;
 var goldPerSecond = 0;
 var goldPerClick = 1;
 
-var clickerCost = Math.floor(10 * Math.pow(1.1,clickers));
-var fastClickerCost = Math.floor(50 * Math.pow(1.1,fastClickers));
+var clickerCost = Math.floor(10 * Math.pow(1.1, clickers));
+var fastClickerCost = Math.floor(50 * Math.pow(1.1, fastClickers));
 
-var manualUpgradeCost = Math.floor(5 * Math.pow(1.05,manualUpgrades+1));
+var manualUpgradeCost = Math.floor(5 * Math.pow(1.05, manualUpgrades + 1));
 
 
 document.getElementById("clickerCost").innerHTML = clickerCost;
@@ -20,90 +20,92 @@ document.getElementById("manualUpgradeCost").innerHTML = manualUpgradeCost;
 
 
 //function for adding gold
-function giveGold(number){
-	gold = gold + number;
-	document.getElementById("gold").innerHTML = Math.round(gold);
+function giveGold(number) {
+    gold = gold + number;
+    document.getElementById("gold").innerHTML = Math.round(gold);
 };
 
 //function for manual clicking
-function clickGold(){
-	giveGold(goldPerClick);
+function clickGold() {
+    giveGold(goldPerClick);
 }
 
 //Function for first Clicker
-function buyClicker(){
-	var clickerCost = Math.floor(10 * Math.pow(1.1,clickers));
-	if (gold >= clickerCost){
-		clickers = clickers + 1;
-		gold = gold - clickerCost;
-		document.getElementById("clickers").innerHTML = clickers;
-		document.getElementById("gold").innerHTML = gold;};
-	var clickerCost = Math.floor(10 * Math.pow(1.1,clickers));
-	document.getElementById("clickerCost").innerHTML = clickerCost;
-	calculateGPS();
+function buyClicker() {
+    var clickerCost = Math.floor(10 * Math.pow(1.1, clickers));
+    if (gold >= clickerCost) {
+        clickers = clickers + 1;
+        gold = gold - clickerCost;
+        document.getElementById("clickers").innerHTML = clickers;
+        document.getElementById("gold").innerHTML = gold;
+    };
+    var clickerCost = Math.floor(10 * Math.pow(1.1, clickers));
+    document.getElementById("clickerCost").innerHTML = clickerCost;
+    calculateGPS();
 };
 
 //Function for second Clicker
-function buyFastClicker(){
-	var fastClickerCost = Math.floor(50 * Math.pow(1.1,fastClickers));
-	if (gold >= fastClickerCost){
-		fastClickers = fastClickers + 1;
-		gold = gold - fastClickerCost;
-		document.getElementById("fastClickers").innerHTML = fastClickers;
-		document.getElementById("gold").innerHTML = gold;};
-	var fastClickerCost = Math.floor(50 * Math.pow(1.1,fastClickers));
-	document.getElementById("fastClickerCost").innerHTML = fastClickerCost;
-	calculateGPS();
+function buyFastClicker() {
+    var fastClickerCost = Math.floor(50 * Math.pow(1.1, fastClickers));
+    if (gold >= fastClickerCost) {
+        fastClickers = fastClickers + 1;
+        gold = gold - fastClickerCost;
+        document.getElementById("fastClickers").innerHTML = fastClickers;
+        document.getElementById("gold").innerHTML = gold;
+    };
+    var fastClickerCost = Math.floor(50 * Math.pow(1.1, fastClickers));
+    document.getElementById("fastClickerCost").innerHTML = fastClickerCost;
+    calculateGPS();
 };
 
-function buyClickUpgrade(){
-	manualUpgradeCost = Math.floor(5 * Math.pow(1.05,manualUpgrades+1));
-	if (gold >= manualUpgradeCost){
-		manualUpgrades = manualUpgrades + 1;
-		gold = gold - manualUpgradeCost;
-		document.getElementById("gpc").innerHTML = goldPerClick;
-		document.getElementById("gold").innerHTML = gold;};
-		goldPerClick = goldPerClick + 1;
-	manualUpgradeCost = Math.floor(5 * Math.pow(1.05,manualUpgrades+1));
-	document.getElementById("manualUpgradeCost").innerHTML = manualUpgradeCost;
-	
+function buyClickUpgrade() {
+    manualUpgradeCost = Math.floor(5 * Math.pow(1.05, manualUpgrades + 1));
+    if (gold >= manualUpgradeCost) {
+        manualUpgrades = manualUpgrades + 1;
+        gold = gold - manualUpgradeCost;
+        document.getElementById("gpc").innerHTML = goldPerClick;
+        document.getElementById("gold").innerHTML = gold;
+    };
+    goldPerClick = goldPerClick + 1;
+    manualUpgradeCost = Math.floor(5 * Math.pow(1.05, manualUpgrades + 1));
+    document.getElementById("manualUpgradeCost").innerHTML = manualUpgradeCost;
+
 };
 
 //recalculate Gold per second and update screen
-function calculateGPS(){
-		goldPerSecond =
-			(clickers * 1) +
-			(fastClickers * 10);
-		document.getElementById("gps").innerHTML = goldPerSecond;
+function calculateGPS() {
+    goldPerSecond =
+        (clickers * 1) +
+        (fastClickers * 10);
+    document.getElementById("gps").innerHTML = goldPerSecond;
 };
 
-window.setInterval(function(){
-	giveGold(goldPerSecond/4);
-	if (gold >= manualUpgradeCost){
-		document.getElementById("clickUpgrade1").style.color = 'black';
-	}
-	else {
-		document.getElementById("clickUpgrade1").style.color = 'red';
-	}
-	
-	if (gold >= clickerCost){
-		document.getElementById("autoUpgrade1").style.color = 'black';
-	}
-	else {
-		document.getElementById("autoUpgrade1").style.color = 'red';
-	}
+window.setInterval(function () {
+    giveGold(goldPerSecond / 4);
+    if (gold >= manualUpgradeCost) {
+        document.getElementById("clickUpgrade1").style.color = 'black';
+    }
+    else {
+        document.getElementById("clickUpgrade1").style.color = 'red';
+    }
 
-	if (gold >= fastClickerCost){
-		document.getElementById("autoUpgrade2").style.color = 'black';
-	}
-	else {
-		document.getElementById("autoUpgrade2").style.color = 'red';
-	}
+    if (gold >= clickerCost) {
+        document.getElementById("autoUpgrade1").style.color = 'black';
+    }
+    else {
+        document.getElementById("autoUpgrade1").style.color = 'red';
+    }
+
+    if (gold >= fastClickerCost) {
+        document.getElementById("autoUpgrade2").style.color = 'black';
+    }
+    else {
+        document.getElementById("autoUpgrade2").style.color = 'red';
+    }
 
 }, 250);
-		
 
-	
-		
-		
-		
+
+
+
+
